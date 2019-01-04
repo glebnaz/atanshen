3d0ea34f3847d3859538de66e4e06628d5fe5dd2
Kirill Chirkin <geimerchannel@gmail.com>
2019-01-04 16:58:19 +0300

    Рефакторинг парсера, добавлен парсинг класса post bg2

--- main.go
+++ main.go
@@ -6,42 +6,77 @@ import (
 	"strings"
 )
 
+/**
+	Иерархия html документа, по которой производится поиск
+
+	div class=post bg1 or class=post bg2
+	div class=inner
+	div class=postbody
+	div class=content <<- здесь лежит нужное сообщение и иногда лишний html код (например, картинка)
+ */
+
 func main() {
-	doc, err := goquery.NewDocument("https://forum.awd.ru/viewtopic.php?f=326&t=326384&start=99999999999999")
+	//"https://forum.awd.ru/viewtopic.php?f=326&t=326384&start=15900" <<- на этой странице "есть места", можно использовать для проверки парсера
+	htmlDoc, err := goquery.NewDocument("https://forum.awd.ru/viewtopic.php?f=326&t=326384&start=99999999999999") // start=99999999999999 написано для того, чтобы скрипт всегда попадал на последнюю страницу форума
 	if err != nil {
 		fmt.Printf("Err %v\n", err)
+		//TODO send to user
 	}
 
-	doc.Find("div").Each(func(index int, s *goquery.Selection) {
-		// получаем ссылку из атрибута
-		if class, ok := s.Attr("class"); ok && class == "post bg1" {
-			ch := s.Children()
-			ch.Each(func(index int, s *goquery.Selection) {
-				if class, ok := s.Attr("class"); ok && class == "inner" {
-					ch := s.Children()
-					ch.Each(func(index int, s *goquery.Selection) {
-						if class, ok := s.Attr("class"); ok && class == "postbody" {
-							ch := s.Children()
-							ch.Each(func(index int, s *goquery.Selection) {
-								if class, ok := s.Attr("class"); ok && class == "content" {
-									html, _ := s.Html()
-									//fmt.Println(html)
-									htmlLow:=strings.ToLower(html)
-									have := strings.Contains(htmlLow,"мест нет")
-									if !have {
-										have = strings.Contains(htmlLow,"нет мест")
-										if !have {
-											fmt.Println("AHTUNG")
-											fmt.Println(strings.TrimSpace(html))
-										}
-									}
-								}
-							})
-						}
-					})
-				}
-			})
+	htmlDoc.Find("div").Each(onDivFound) //начинаем парсинг
+}
+
+func onDivFound(_ int, selection *goquery.Selection) {
+	class, ok := selection.Attr("class")
+	if ok && (class == "post bg1" || class == "post bg2") {
+		onPostBgFound(selection)
+	}
+}
+
+func onPostBgFound(selection *goquery.Selection) {
+	children := selection.Children()
+	children.Each(func(_ int, selection *goquery.Selection) {
+		class, ok := selection.Attr("class")
+		if ok && class == "inner" {
+			onInnerFound(selection)
 		}
 	})
+}
 
+func onInnerFound(selection *goquery.Selection) {
+	children := selection.Children()
+	children.Each(func(_ int, selection *goquery.Selection) {
+		class, ok := selection.Attr("class")
+		if ok && class == "postbody" {
+			onPostbodyFound(selection)
+		}
+	})
+}
+
+func onPostbodyFound(selection *goquery.Selection) {
+	children := selection.Children()
+	children.Each(func(_ int, selection *goquery.Selection) {
+		class, ok := selection.Attr("class")
+		if ok && class == "content" {
+			onContentFound(selection)
+		}
+	})
+}
+
+func onContentFound(selection *goquery.Selection) {
+	html, _ := selection.Html()
+	htmlLowerCase := strings.ToLower(html)
+	contains := strings.Contains(htmlLowerCase, "мест нет")
+	if !contains {
+		contains = strings.Contains(htmlLowerCase, "нет мест")
+		if !contains {
+			//значит есть места или кто-то решил написать на форуме что-то особенное (что маловероятно)
+
+			vitalMessage := strings.TrimSpace(html)
+			fmt.Println("АТАНШЕН")
+			fmt.Println(vitalMessage)
+			fmt.Println()
+			//TODO send message to user
+		}
+	}
 }
